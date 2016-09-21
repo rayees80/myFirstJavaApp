6647750a8a86e0aec9b1fdeb73f8d4bc3a752afa
Rayees Afroz <rayees80@squ.edu.om>
2016-09-21 14:27:12 +0400

    corrected hello world stmt

--- src/myfirstjavaapp/MyFirstJavaApp.java
+++ src/myfirstjavaapp/MyFirstJavaApp.java
@@ -15,7 +15,7 @@ public class MyFirstJavaApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Worl!");
+        System.out.println("Hello World!");
     }
     
 }
